b0cd576b4fb4e44eefd9810ec649e59a2ad232f0
guillaume <dumont.guillaume@gmail.com>
2015-05-31 22:17:27 -0400

    Added precompiled OpenBLAS

---
+++ openblas/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.14")
+SET(OpenBLAS_INCLUDE_DIRS /home/traits/project/pack_openblas/OpenBLAS-v0.2.14-Win64-int32/include)
+SET(OpenBLAS_LIBRARIES /home/traits/project/pack_openblas/OpenBLAS-v0.2.14-Win64-int32/bin/libopenblas.dll)
